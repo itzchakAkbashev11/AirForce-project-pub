31ad1f0deddbad96e1f68aee324049d0ff0cc271
itzchakAkbashev11 <156307797+itzchakAkbashev11@users.noreply.github.com>
2024-04-03 00:40:24 +0300

    Add files via upload

--- AirServer/app.js
+++ AirServer/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
 const path = require('path');
+
 const http = require('http');
+// cors- can requested from one domain and used by another domain
 const cors = require('cors');
 require('./db/mongoConnect');
 const { routesInit } = require('./routs/configRouts');
 const app = express();
+//  This allows requests from any origin (any domain)
+// This server is willing to accept credentials such as cookies or authorization headers from the client
 app.use(cors(
   {
     origin:true,
